33123cf84792fb42bbf1332409ed7983127447da
ilius33 <ilius33@mail.ru>
2017-02-24 09:55:22 +0300

    Institution model

---
+++ app/Models/Institution.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Institution extends Model
+{
+    /*
+    	Тип:
+    		Учреждение
+
+    	Поля:
+    		Название
+    */
+    /**
+    * Массово присваиваемые атрибуты.
+    *
+    * @var array
+    */
+    protected $fillable = ['name', 'region_id'];
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function districts()
+	{
+	    return $this->belongsToMany(District::class);
+	}
+}
