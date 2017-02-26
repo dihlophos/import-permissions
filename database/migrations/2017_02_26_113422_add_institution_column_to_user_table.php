c2e9bc060ed86feae603f9cd34bfb3421e191ecc
Alex My <alexmy@outlook.com>
2017-02-26 15:52:43 +0300

    Institution column migration fix

--- database/migrations/2017_02_26_113422_add_institution_column_to_user_table.php
+++ database/migrations/2017_02_26_113422_add_institution_column_to_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddInstitutionColumtToUserTable extends Migration
+class AddInstitutionColumnToUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddInstitutionColumtToUserTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('institution_id')->index();
+            $table->integer('institution_id')->nullable()->index();
         });
     }
 
