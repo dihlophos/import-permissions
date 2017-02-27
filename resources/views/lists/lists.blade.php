e7d672c366043e5b451957c2573ead15ce8c9a01
dihlophos <dihlophos2@gmail.com>
2017-02-27 10:49:15 +0400

    Lists index update

--- resources/views/lists/lists.blade.php
+++ resources/views/lists/lists.blade.php
@@ -8,12 +8,13 @@
         <div class="panel-heading">Справочники</div>
         <div class="panel-body">
             <ul>
-               <li><a href="{{ URL::action('OrganizationController@index') }}">Организации</a></li>
-               <li><a href="{{ URL::action('RegionController@index') }}">Регионы/районы</a></li>
-               <li><a href="{{ URL::action('TransportController@index') }}">Транспорт</a></li>
-               <li><a href="{{ URL::action('PurposeController@index') }}">Цель вывоза</a></li>
-               <li><a href="{{ URL::action('ProductTypeController@index') }}">Виды грузов<a></li>
-               <li><a href="{{ URL::action('StorageController@index') }}">Базы хранения</a></li>
+               <li><a href="{{ route('user'.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('organization'.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('region'.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('transport'.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('purpose'.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('product_type.index') }}">Учреждения</a></li>
+               <li><a href="{{ route('storage.index') }}">Учреждения</a></li>
                <li><a href="{{ route('institution.index') }}">Учреждения</a></li>
             </ul>
         </div>
