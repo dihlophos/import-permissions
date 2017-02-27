07c57c5362d7a0c4604bf9462259519e9f17817e
dihlophos <dihlophos2@gmail.com>
2017-02-27 11:17:29 +0400

    Lists index fix

--- resources/views/lists/lists.blade.php
+++ resources/views/lists/lists.blade.php
@@ -8,11 +8,11 @@
         <div class="panel-heading">Справочники</div>
         <div class="panel-body">
             <ul>
-               <li><a href="{{ route('user'.index') }}">Пользователи</a></li>
-               <li><a href="{{ route('organization'.index') }}">Организации</a></li>
-               <li><a href="{{ route('region'.index') }}">Регионы/районы</a></li>
-               <li><a href="{{ route('transport'.index') }}">Транспорт</a></li>
-               <li><a href="{{ route('purpose'.index') }}">Цель вывоза</a></li>
+               <li><a href="{{ route('user.index') }}">Пользователи</a></li>
+               <li><a href="{{ route('organization.index') }}">Организации</a></li>
+               <li><a href="{{ route('region.index') }}">Регионы/районы</a></li>
+               <li><a href="{{ route('transport.index') }}">Транспорт</a></li>
+               <li><a href="{{ route('purpose.index') }}">Цель вывоза</a></li>
                <li><a href="{{ route('product_type.index') }}">Виды грузов</a></li>
                <li><a href="{{ route('storage.index') }}">Базы хранения</a></li>
                <li><a href="{{ route('institution.index') }}">Учреждения</a></li>
